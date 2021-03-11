a7fb08d983631ea802ad3114120938a6cc9636bd
root <ncodeit.github@gmail.com>
2021-03-11 09:16:03 +0000

    s1.sh

--- s1.sh
+++ s1.sh
@@ -1,7 +1,6 @@
-ping google.com
-ping ncodeit.com
-ping gitlab.com
-ls
-cat /etc
-find /sbin -mtime 5
-
+ping -c 2 google.com
+ping -c 2 ncodeit.com
+ping -c 2 github.com
+df -k
+find /home/etc -name *
+find /home/sbin -mtime +5
