daf9a4e7fc8bb80f3704b78370abd6027aa7ca7c
root <root@ncduat3srvr1.lxd>
2021-04-10 05:13:19 +0000

    Team3.sh

---
+++ Team3.sh
@@ -0,0 +1,4 @@
+ping the sites google.com, ncodeit.com, gitlab.com
+display the list of files systems on this systems
+list all the files in /etc directory
+find all the files in /sbin which are older than 5 days
